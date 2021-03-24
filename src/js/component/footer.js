270d06988245f30683c9421bb19db1bc4c365e59
Kamari1974 <karen197499@gmail.com>
2021-03-24 02:06:39 +0000

    trabajo terminado

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div>
+			<nav className="navbar navbar-dark bg-dark">
+				Copyright Your Website 2018
+			</nav>
+		</div>
+	);
+}
